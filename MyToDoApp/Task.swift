fcde2cdba75b811229f05254fa8e80e7aee248bd
yoshi104 <yoshiyukikt@gmail.com>
2021-03-27 22:15:49 +0900

    Upadte: TaskのEntityを修正

---
+++ MyToDoApp/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  MyToDoApp
+//
+//  Created by Yoshiyuki Kato on 2021/03/25.
+//
+
+import Foundation
+
+struct Task: Codable {
+    let id: String
+    let title: String
+}
+
